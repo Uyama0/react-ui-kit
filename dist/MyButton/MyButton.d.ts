d0ad059272dd7d30de7ba5209805702c10a924f6
Ruslan <axelezgames@gmail.com>
2023-10-20 18:46:53 +0500

    add: css styles

--- dist/MyButton/MyButton.d.ts
+++ dist/MyButton/MyButton.d.ts
@@ -1,6 +1,8 @@
 import React from "react";
+import "./MyButton.css";
 export interface MyButtonProps {
     color: string;
+    medium?: boolean;
 }
-declare function MyButton({ color, ...props }: MyButtonProps): React.JSX.Element;
+declare function MyButton({ color, medium, ...props }: MyButtonProps): React.JSX.Element;
 export default MyButton;
--- src/MyButton/MyButton.tsx
+++ src/MyButton/MyButton.tsx
@@ -1,14 +1,25 @@
 import React, { FC } from "react";
 
+import "./MyButton.css"
+
 export interface MyButtonProps {
   color: string;
+  medium?: boolean;
 }
 
-function MyButton({ color, ...props }: MyButtonProps) {
+function MyButton({ color, medium, ...props }: MyButtonProps) {
+  const rootClasses = ["my-button"]
+
+  if (medium) {
+    rootClasses.push("medium")
+  }
+
   return (
-    <button {...props} style={{ color }}>
-      hello
-    </button>
+    <div>
+      <button {...props} className={rootClasses.join(" ")} style={{ color }}>
+        hello
+      </button>
+    </div>
   );
 }
 
